6d56cf799b10ec23c7f80f7a879c4a07fbbecee6
terra819 <mrmrsgilbert@gmail.com>
2019-04-12 19:49:26 -0400

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { tileLayer, latLng, divIcon, marker, polygon, circle, icon } from 'leaflet';
+// var json = require('../assets/meetups.json');
 
 @Component({
 	selector: 'my-app',
@@ -9,16 +10,23 @@ import { tileLayer, latLng, divIcon, marker, polygon, circle, icon } from 'leafl
 export class AppComponent {
 	redCircleIcon = divIcon({
 		className: 'center',
-		html: '<div class="animate zoomIn fadeIn fadeOut"><img src="https://cdn.jsdelivr.net/gh/terra819/animated-meetup-map@master/src/assets/redcircle.png" height="50" width="50"></div>',
+		html: '<div class="animate zoomIn"><img src="https://cdn.jsdelivr.net/gh/terra819/animated-meetup-map@master/src/assets/redcircle.png" height="50" width="50"></div>',
 	});
 	options = {
 		layers: [
-			tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+			tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
 		],
 		zoom: 5,
 		center: latLng(46.879966, -121.726909)
 	};
-	layers = [
+  layers = [
 		marker([46.879966, -121.726909], { icon: this.redCircleIcon })
 	];
+  // meetups = json.meetups;
+
+  // constructor() {
+  //   for(let meetup of this.meetups){
+  //     this.layers.push(marker([meetup.lat, meetup.lon], { icon: this.redCircleIcon }));
+  //   }
+  // }
 }
