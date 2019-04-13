9b29f18ec5172b4bfd981e4b016ffb5383d082cc
terra819 <mrmrsgilbert@gmail.com>
2019-04-12 21:08:22 -0400

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,28 +3,53 @@ import { tileLayer, latLng, divIcon, marker, polygon, circle, icon } from 'leafl
 var json = require('../assets/meetups.json');
 
 @Component({
-	selector: 'my-app',
-	templateUrl: './app.component.html',
-	styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	redCircleIcon = divIcon({
-		className: 'center',
-		html: '<div class="animate zoomIn"><img src="https://cdn.jsdelivr.net/gh/terra819/animated-meetup-map@master/src/assets/redcircle.png" height="50" width="50"></div>',
-	});
-	options = {
-		layers: [
-			tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
-		],
-		zoom: 5,
-		center: latLng(46.879966, -121.726909)
-	};
+  redCircleIcon = divIcon({
+    className: 'center',
+    html: '<div class="elementToFadeInAndOut"><img src="https://cdn.jsdelivr.net/gh/terra819/animated-meetup-map@master/src/assets/redcircle.png" height="50" width="50"></div>',
+  });
+  options = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    ],
+    zoom: 5,
+    center: latLng(46.879966, -121.726909)
+  };
   layers = [];
   meetups = json.meetups;
+  playing = false;
+  interval: any;
+  playerDate = new Date('4/3/2019');
 
-  constructor() {
-    for(let meetup of this.meetups){
-      this.layers.push(marker([meetup.lat, meetup.lon], { icon: this.redCircleIcon }));
+  constructor() { }
+
+  play() {
+    this.playing = true;
+    const _this = this;
+    this.interval = setInterval(function () { _this.plot(); }, 500);
+  }
+
+  stop() {
+    this.playing = false;
+    clearInterval(this.interval);
+    this.playerDate = new Date('4/3/2019');
+  }
+
+  plot() {
+    // this.layers = [];
+    for (let meetup of this.meetups) {
+      const date1 = new Date(meetup.date);
+      if (date1.getTime() === this.playerDate.getTime()) {
+        this.layers.push(marker([meetup.lat, meetup.lon], { icon: this.redCircleIcon }));
+      }
     }
+    this.playerDate = new Date(
+      this.playerDate.getFullYear(),
+      this.playerDate.getMonth(),
+      this.playerDate.getDate() + 1);
   }
 }
